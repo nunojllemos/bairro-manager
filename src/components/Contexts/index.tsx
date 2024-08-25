8c22c272cf0bf7f999aefcb6394a536a19c97314
Nuno <nunojllemos@gmail.com>
2024-08-25 18:43:47 +0100

    Add MUI integration with next js

--- src/components/Contexts/index.tsx
+++ src/components/Contexts/index.tsx
@@ -1,16 +1,19 @@
 'use client'
 import React from 'react'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
 import AuthContextProvider from '@/context/AuthContext'
 import PlayersContextProvider from '@/context/PlayersContext'
 import FinesContextProvider from '@/context/FinesContext'
 
 const Contexts = ({ children }: { children: React.ReactNode }) => {
     return (
-        <AuthContextProvider>
-            <PlayersContextProvider>
-                <FinesContextProvider>{children}</FinesContextProvider>
-            </PlayersContextProvider>
-        </AuthContextProvider>
+        <AppRouterCacheProvider>
+            <AuthContextProvider>
+                <PlayersContextProvider>
+                    <FinesContextProvider>{children}</FinesContextProvider>
+                </PlayersContextProvider>
+            </AuthContextProvider>
+        </AppRouterCacheProvider>
     )
 }
 
