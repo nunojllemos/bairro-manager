1b93c9fb7176f16c4022771fe9147201cda7cded
nunojllemos <nunojllemos@gmail.com>
2024-09-02 16:51:04 +0100

    Trying to revalidate data

--- src/app/api/players/route.ts
+++ src/app/api/players/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import PlayerModel from '@/models/player'
 import connectDB from '@/lib/db'
 
-export const revalidate = 60
+export const revalidate = 30
 
 export async function GET() {
     await connectDB()
